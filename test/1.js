9a8176c22ed2f8f97310899119bae7165ba29627
minh1304 <vtm1304@gmail.com>
2022-08-07 14:43:54 +0700

    Update 1.js

--- test/1.js
+++ test/1.js
@@ -1,7 +1,25 @@
 x = 10;
 y= 20;
 z = 1;
-function chaythu() {
+function chaythu(){
     console.log('ket qua la: ' + z);
     z +=1;
 }
+
+// object
+var hocvien = {
+    ten : 'Minh',
+    ho : 'Vo',
+    tuoi: 21,
+    chieucao: 170,
+    nang: 60,
+    hienthi :function(){
+
+        console.log(this.ho + " " + this.ten);
+
+
+    }
+    
+
+};
+hocvien.hienthi();
