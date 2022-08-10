bd9bd9826bb322eeecd8f8fd58f2dbafba9e0747
minh1304 <vtm1304@gmail.com>
2022-08-10 22:08:37 +0700

    upload

---
+++ BT1/1.js
@@ -0,0 +1,7 @@
+var x = document.getElementsByTagName('h4');
+for ( var i =0; i< x.length; i++)
+{
+    x[i].innerHTML = "<button type='button' class='btn btn-primary btn-block'></button>"
+    
+}
+
