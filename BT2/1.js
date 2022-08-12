4700113934b4000c65e5a8f63cb4ddc6e047056f
minh1304 <vtm1304@gmail.com>
2022-08-12 16:19:52 +0700

    Update 1.js

--- BT2/1.js
+++ BT2/1.js
@@ -1,11 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var nut = document.getElementById('nut1');
-    var khoi = document.getElementsByClassName("card")
-    nut.onclick = function() {
-        khoi[0].classList.toggle('diqua')
-    
+document.addEventListener("DOMContentLoaded",function(){
+    var nut = document.getElementById("nut1");
+    var trangthai = "lan1";
+    var bang =document.getElementsByClassName("card");
+    nut.onclick = function(){
+        if (trangthai=="lan1") {
+            console.log("click lan 1");
+            trangthai = "lan2";
+        }
+        else if(trangthai=="lan2")
+        {
+            console.log("click lan 2");
+            trangthai = "lan1";
+        }
     }
 
-
 },false)
-
