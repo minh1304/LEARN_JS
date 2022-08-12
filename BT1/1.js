b06195bdbe9c61c1a5fc12671ce0409e3855900c
minh1304 <vtm1304@gmail.com>
2022-08-12 15:03:50 +0700

    upload

--- BT1/1.js
+++ BT1/1.js
@@ -1,10 +1,11 @@
-document.addEventListener("DOMContentLoaded",function(){
-
-    var x1= document.getElementById('n1');
-    console.log(x1);
-    x1.onclick = function(){
-        console.log('ban vua click');
-        x1.classList.add('dixuong')
+document.addEventListener("DOMContentLoaded", function(){
+    var nut = document.getElementById('nut1');
+    var khoi = document.getElementsByClassName("card")
+    nut.onclick = function() {
+        khoi[0].classList.toggle('diqua')
+    
     }
 
+
 },false)
+
