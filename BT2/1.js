07fae1510e17e1962a7ba7bc5a3fd3f0c5cb9deb
minh1304 <vtm1304@gmail.com>
2022-08-12 16:52:46 +0700

    upload

--- BT2/1.js
+++ BT2/1.js
@@ -1,16 +1,22 @@
 document.addEventListener("DOMContentLoaded",function(){
     var nut = document.getElementById("nut1");
+    var khoi = document.getElementById("kcd");
     var trangthai = "lan1";
     var bang =document.getElementsByClassName("card");
     nut.onclick = function(){
         if (trangthai=="lan1") {
             console.log("click lan 1");
             trangthai = "lan2";
+            khoi.classList.remove("ko-dong");
+            khoi.classList.add("chieu1");
+
         }
         else if(trangthai=="lan2")
         {
             console.log("click lan 2");
             trangthai = "lan1";
+            khoi.classList.remove("chieu1");
+
         }
     }
 
