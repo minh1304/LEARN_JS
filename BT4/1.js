2246523a7b4652e34de744695b4487837b90a576
minh1304 <vtm1304@gmail.com>
2022-08-13 17:49:50 +0700

    upload

--- BT4/1.js
+++ BT4/1.js
@@ -3,7 +3,15 @@ document.addEventListener("DOMContentLoaded",function(){
     var nut= document.getElementsByClassName("declick");
     for (var i = 0; i < nut.length; i++) {
         nut[i].onclick = function(){
-            this.classList.toggle("mautrang")
+            for (k= 0; k< nut.length; k++) {
+                nut[k].classList.remove("mautrang")
+                
+            }
+            this.classList.toggle("mautrang");
+            //lấy về data-hiện lên
+            var ndhienra = this.getAttribute("data-hienlen");
+            var phan_tu_hien_ra = document.getElementById(ndhienra);
+            phan_tu_hien_ra.classList.add('xuathien');
         }
         
     }
