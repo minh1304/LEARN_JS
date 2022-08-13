f10d0db5ca4748c69b9cabbca357feb88f5c2efa
minh1304 <vtm1304@gmail.com>
2022-08-13 17:19:13 +0700

    upload

--- BT4/1.js
+++ BT4/1.js
@@ -1,29 +1,12 @@
 document.addEventListener("DOMContentLoaded",function(){
-    var tg = document.getElementById("tg");
-    var ds = document.getElementById("ds");
-    // var trangthai = "lan1";
-    tg.onclick = function(){
-        // if (trangthai=="lan1")
-        // {
-        //     console.log("click lan 1");
-        //     this.style.color = 'white';
-        //     ds.classList.add('xuathien');
 
-        //     trangthai = "lan2"
-        // }
-        // else if (trangthai=="lan2")
-        // {
-        //     console.log("click lan 2");
-        //     //cach moi
-        //     this.style.color = 'black';
-        //     ds.classList.remove('xuathien');
-                        
-        //     trangthai = "lan1"
-        // }
-        this.classList.toggle('tamgiactrang');
-        ds.classList.toggle('xuathien');
-
-
+    var nut= document.getElementsByClassName("declick");
+    for (var i = 0; i < nut.length; i++) {
+        nut[i].onclick = function(){
+            this.classList.toggle("mautrang")
+        }
+        
     }
+
 
 },false)
