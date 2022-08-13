3335f031834f6364be1ff657113f41a96b28a01c
minh1304 <vtm1304@gmail.com>
2022-08-13 18:11:04 +0700

    Update 1.js

--- BT4/1.js
+++ BT4/1.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded",function(){
 
     var nut= document.getElementsByClassName("declick");
+    var nd = document.getElementsByClassName("danhsach");
     for (var i = 0; i < nut.length; i++) {
         nut[i].onclick = function(){
             for (k= 0; k< nut.length; k++) {
@@ -11,7 +12,12 @@ document.addEventListener("DOMContentLoaded",function(){
             //lấy về data-hiện lên
             var ndhienra = this.getAttribute("data-hienlen");
             var phan_tu_hien_ra = document.getElementById(ndhienra);
-            phan_tu_hien_ra.classList.add('xuathien');
+            for (var i= 0;i < nd.length; i++) {
+                nd[i].classList.remove('xuathien');
+                
+            }
+
+            phan_tu_hien_ra.classList.toggle('xuathien');
         }
         
     }
