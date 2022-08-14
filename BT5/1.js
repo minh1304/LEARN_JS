02469e3fd9457d2a71eb1c3bc8eaac146bac0420
minh1304 <vtm1304@gmail.com>
2022-08-14 14:12:44 +0700

    upload

--- BT5/1.js
+++ BT5/1.js
@@ -1,15 +1,21 @@
 document.addEventListener("DOMContentLoaded",function(){
     var nut = document.querySelector('.nut'),
         menutrai = document.querySelector('.menutrai'),
-        black = document.querySelector('.black');
-        khoito = document.querySelector('.content');
+        black = document.querySelector('.black'),
+        khoito = document.querySelector('.content'),
+        trang = document.querySelector('.mautrang');
     nut.onclick = function(){
         black.classList.add('len');
-        khoito.classList.add('dichphai');
+        // khoito.classList.add('dichphai');
+        trang.classList.add('trangquay');
+        menutrai.classList.remove('vetrai');
     }
     black.onclick = function(){
         black.classList.remove('len');
         khoito.classList.remove('dichphai');
+        trang.classList.remove('trangquay');
+        menutrai.classList.add('vetrai');
+
 
     }
 
