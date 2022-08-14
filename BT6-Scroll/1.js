c53e0d63c6066884a3ba240d77b54a46780d1f58
minh1304 <vtm1304@gmail.com>
2022-08-14 16:10:33 +0700

    upload

--- BT6-Scroll/1.js
+++ BT6-Scroll/1.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded",function(){
+    var trangthai ='duoi300'; //kh cần cũng đc 
+    
+    var menu = document.querySelector('.menu');
     window.addEventListener('scroll',function(){
-        console.log("cuon roi");
+        if(window.pageYOffset >300){
+            if (trangthai=='duoi300') {
+                trangthai="tren300";
+                menu.classList.add('nholai');
+                
+            }
+
+        }
+        else if(window.pageYOffset <=300)
+        {
+            if(trangthai="tren300")
+            {
+                menu.classList.remove('nholai');
+                trangthai="duoi300";
+            }
+        }
     })
 
 },false)
