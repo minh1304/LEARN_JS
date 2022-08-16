30b5f1534ec18bf6d345452a9feefe27ad5bc6b3
minh1304 <vtm1304@gmail.com>
2022-08-16 15:47:31 +0700

    upload

--- BT7-Scroll2/1.js
+++ BT7-Scroll2/1.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded",function(){
     var menudo = document.querySelector('.menu');
     var trangthaimenudo = "duoi150";
+    var quangcao = document.querySelector('.quangcao');
+    var vitriqc = quangcao.offsetTop; 
+    var vitriqccuoi = vitriqc + 700;
+    // console.log(vitriqc);
+    var trangthaiqc = "duoi";
     window.addEventListener('scroll',function(){
         // console.log(window.pageYOffset);
         if(this.window.pageYOffset > 150)
@@ -24,6 +29,31 @@ document.addEventListener("DOMContentLoaded",function(){
             }
 
         }
+        
+        //xu ly quang cao
+        if( (this.window.pageYOffset > vitriqc )  && (this.window.pageYOffset < vitriqccuoi )) 
+        {
+            if(trangthaiqc=="duoi")
+            {
+                trangthaiqc = "danghienthi";
+                quangcao.classList.add('hienqc');
+
+            }   
+        }
+        else if ((this.window.pageYOffset < vitriqc ) || (this.window.pageYOffset > vitriqccuoi ))
+        {
+            if(trangthaiqc=="danghienthi")
+            {
+                trangthaiqc = "duoi";
+                quangcao.classList.remove('hienqc');
+
+            }  
+        }
+
+
     })
+
+
+
 
 },false)
