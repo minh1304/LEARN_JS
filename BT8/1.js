7e2f047728733a3426ffde48fb2eed6d93ba6f12
minh1304 <vtm1304@gmail.com>
2022-08-16 20:05:01 +0700

    upload
    cho slide chuyển động khi click vào nút

--- BT8/1.js
+++ BT8/1.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded",function(){
     var click = document.querySelectorAll("li");
+    var slides = document.querySelectorAll(".cacslide ul li")
+    // console.log(slides);
 for (var i = 0; i < click.length; i++) {
     click[i].addEventListener('click',function(){
         for (var i = 0; i < click.length; i++) {
@@ -7,6 +9,26 @@ for (var i = 0; i < click.length; i++) {
         }
 
         this.classList.add('kichhoat');
+
+        // Hàm tính vị trí nút
+        var nutkichhoat = this ;
+        for(var vitrinut = 0; nutkichhoat = nutkichhoat.previousElementSibling;vitrinut++)
+        {
+
+        }
+        for(var i =0 ; i < slides.length; i++)
+        {
+            slides[i].classList.remove('active');
+            slides[vitrinut].classList.add('active');
+        }
+
+
+        // for (var i = 0; i < slides.length; i++) {
+        //     slides[i].classList.remove('active');
+
+
+            
+        // }
 
     })
 }
