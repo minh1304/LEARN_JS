97b2e031064f91101d59e5ec62ea0c4bda49ea06
minh1304 <vtm1304@gmail.com>
2022-08-16 15:10:16 +0700

    upload

--- BT7-Scroll2/1.js
+++ BT7-Scroll2/1.js
@@ -1,4 +1,29 @@
 document.addEventListener("DOMContentLoaded",function(){
-    
+    var menudo = document.querySelector('.menu');
+    var trangthaimenudo = "duoi150";
+    window.addEventListener('scroll',function(){
+        // console.log(window.pageYOffset);
+        if(this.window.pageYOffset > 150)
+        {
+            if (trangthaimenudo == "duoi150")
+            {
+                trangthaimenudo = "tren150";
+                menudo.classList.add('menuden');
+
+            }
+
+            
+        }
+        else if(this.window.pageYOffset < 150)
+        {
+            if (trangthaimenudo == "tren150")
+            {
+                trangthaimenudo = "duoi150";
+                menudo.classList.remove('menuden');
+
+            }
+
+        }
+    })
 
 },false)
