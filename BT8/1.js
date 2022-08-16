eca56fec6b2669f2b612d28124c75ad14cf458bf
minh1304 <vtm1304@gmail.com>
2022-08-16 16:55:01 +0700

    upload
    chỉnh hiệu ứng cho nút

--- BT8/1.js
+++ BT8/1.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded",function(){
+    var click = document.querySelectorAll("li");
+for (var i = 0; i < click.length; i++) {
+    click[i].addEventListener('click',function(){
+        for (var i = 0; i < click.length; i++) {
+            click[i].classList.remove('kichhoat');
+        }
+
+        this.classList.add('kichhoat');
+
+    })
+}
 
 },false)
