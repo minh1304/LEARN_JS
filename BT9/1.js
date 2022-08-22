0345b499f6c53d200010a9f4d144e8316478d738
minh1304 <vtm1304@gmail.com>
2022-08-22 14:57:06 +0700

    Update 1.js

--- BT9/1.js
+++ BT9/1.js
@@ -1,6 +1,26 @@
 document.addEventListener("DOMContentLoaded",function(){
-    //bắt sự kiện click vào nút 
-    var nutNext = document.querySelector('.nut b.phai')
-    console.log(nutNext);
+
+    var nutPhai = document.querySelector('.nut b.phai'),
+        nutTrai = document.querySelector('.nut b.trai'),
+        slides = document.querySelectorAll('.slides ul li'),
+        chiSoHientai = 0,
+        soLuongSlide =  slides.length;
+
+    //bắt sự kiện click vào nút phải 
+    var chuyenSlideChoNutPhai = function() {
+        var phanTuHienTai = slides[chiSoHientai];
+        //xác định chỉ số tiếp theo dựa trên hiện tại
+        if (chiSoHientai < soLuongSlide - 1){
+            chiSoHientai++;
+        }
+        else
+        {
+            chiSoHientai = 0;  
+        }
+        var phanTuTiepTheo = slides[chiSoHientai];
+        console.log(phanTuHienTai);
+        console.log(phanTuTiepTheo);
+    };
+    nutPhai.addEventListener('click', chuyenSlideChoNutPhai);
 
 },false)
