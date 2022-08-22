36c4a76b1c97dfbb1048518681489cf0e2469fd7
minh1304 <vtm1304@gmail.com>
2022-08-22 14:25:53 +0700

    upload

--- BT9/1.js
+++ BT9/1.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded",function(){
+    //bắt sự kiện click vào nút 
+    var nutNext = document.querySelector('.nut b.phai')
+    console.log(nutNext);
 
 },false)
