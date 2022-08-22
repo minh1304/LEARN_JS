50f31796aa0d815ef4c35e4540cf3ee5eca30530
minh1304 <vtm1304@gmail.com>
2022-08-22 18:02:41 +0700

    Update 1.js
    chỉnh hiệu ứng cho tnut1 trái

--- BT9/1.js
+++ BT9/1.js
@@ -55,6 +55,55 @@ document.addEventListener("DOMContentLoaded",function(){
         phanTuTiepTheo.classList.add('xuatHienKhiAnNext');
 
     };
+    var chuyenSlideChoNutTrai = function(){
+        //Để kh bị lag khi click nhiều
+        if(trangthai == 'dangChuyenDong')
+        {
+            return false;
+        }
+        trangthai ='dangChuyenDong';
+        var checktrangthai = 0;
+        var phanTuHienTai = slides[chiSoHientai];
+        //xác định chỉ số tiếp theo dựa trên hiện tại
+        if (chiSoHientai > 0){
+            chiSoHientai--;
+        }
+        else
+        {
+            chiSoHientai =  soLuongSlide - 1;  
+        }
+
+        var phanTuTiepTheo = slides[chiSoHientai];
+
+        var xuLyHienTaiketThucCD = function () {
+            // this là phantuhientai
+            this.classList.remove('dangxem');
+            this.classList.remove('bienMatKhiAnNext');
+            checktrangthai ++;
+            if(checktrangthai == 2) {trangthai='dungYen'}
+
+        }
+
+        var xuLyTiepTheoketThucCD = function () {
+            // this là phantuTieptheo
+            this.classList.remove('xuatHienKhiAnNext');
+            this.classList.add('dangxem');
+            checktrangthai ++;
+            if(checktrangthai == 2) {trangthai='dungYen'}
+        }
+
+
+
+        //check chuyển động kết thúc 
+        phanTuHienTai.addEventListener('webkitAnimationEnd', xuLyHienTaiketThucCD);
+        phanTuTiepTheo.addEventListener('webkitAnimationEnd', xuLyTiepTheoketThucCD);
+        //tạo chuyển động sau khi xác định 
+        phanTuHienTai.classList.add('bienMatKhiAnNext');
+        phanTuTiepTheo.classList.add('xuatHienKhiAnNext');
+
+    };
     nutPhai.addEventListener('click', chuyenSlideChoNutPhai);
+    nutTrai.addEventListener('click', chuyenSlideChoNutTrai);
+
 
 },false)
