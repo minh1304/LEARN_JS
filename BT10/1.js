b98001dcc8709189ab8101c09f499300f45ba49c
minh1304 <vtm1304@gmail.com>
2022-08-29 11:59:53 +0700

    upload

--- BT10/1.js
+++ BT10/1.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded",function(){
     var img = document.querySelectorAll('.cacanh img'),
-        black = document.querySelector('.black');
+        black = document.querySelector('.black'),
+        close = document.querySelector('.black .close');
+    console.log(close);
     for (var i = 0; i < img.length; i++) {
         img[i].addEventListener('click',function(){
             console.log(this);
-            black.classList.add('show')
-
+            black.classList.add('show');
+            close.addEventListener('click',function (){
+                black.classList.remove('show');
+            })
+            black.addEventListener('click',function (){
+                black.classList.remove('show');
+            })
         })
 
         
