a0665680cae182324e4a8d62b749378f99309ff7
minh1304 <vtm1304@gmail.com>
2022-08-29 15:22:32 +0700

    Update 1.js

--- BT10/1.js
+++ BT10/1.js
@@ -2,32 +2,40 @@ document.addEventListener("DOMContentLoaded",function(){
     var img = document.querySelectorAll('.cacanh img'),
         black = document.querySelector('.black'),
         info_img = document.querySelector('.info_img'),
-        block_img = document.querySelectorAll('.block_img ul li')
+        block_img = document.querySelectorAll('.block_img ul li'),
+        click_nextImg = document.querySelector('.b_right');
+        // click_previousImg
         close = document.querySelector('.black .close');
-    console.log(close);
-    console.log(block_img);
-    close.addEventListener('click',function (){
-        black.classList.remove('show');
-        info_img.classList.remove('show');
-    })
     
-    black.addEventListener('click',function (){
-        black.classList.remove('show');
-        info_img.classList.remove('show');
-    })
+    console.log(click_nextImg);
     for (var i = 0; i < img.length; i++) {
         img[i].addEventListener('click',function(){
-            console.log(this);
             black.classList.add('show');
             info_img.classList.add('show');
             var imgClick = this;
             var chiSo = 0 ;
             for(chiSo;imgClick=imgClick.previousElementSibling;chiSo++) {}
-            console.log(block_img[chiSo]);
             block_img[chiSo].classList.add('active');
+            close.addEventListener('click',function (){
+                black.classList.remove('show');
+                info_img.classList.remove('show');
+                block_img[chiSo].classList.remove('active');
+            })
+            
+            black.addEventListener('click',function (){
+                black.classList.remove('show');
+                info_img.classList.remove('show');
+                block_img[chiSo].classList.remove('active');
+            })
         })
 
         
     }
+    click_nextImg.addEventListener('click',function(){
+        console.log("click roi");
+        var chiSo = 0 ;
+
+    })
+
 
 },false)
