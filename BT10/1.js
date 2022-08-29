8acf9e2c906884e5490e1c534077d74ef4b5529a
minh1304 <vtm1304@gmail.com>
2022-08-29 10:47:49 +0700

    upload

--- BT10/1.js
+++ BT10/1.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded",function(){
+    var img = document.querySelectorAll('.cacanh');
+    for (var i = 0; i < img.length; i++) {
+        img.addEventListener('click',function(){
+            console.log(this);
+        })
+
+        
+    }
 
 },false)
