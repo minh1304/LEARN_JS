b30b7c1d8c7860a95e7d1de26ef29fcb87a22745
minh1304 <vtm1304@gmail.com>
2022-09-05 22:43:17 +0700

    Update 1.js

--- ES6/arrow_funtion/1.js
+++ ES6/arrow_funtion/1.js
@@ -1,5 +1,7 @@
 //Khai báo một arrow function không truyền vào tham số.
-var getName = () =>  {
-    return "Võ Tuấn Minh"
-}
-console.log(getName());
+    // var getName = () => "Võ Tuấn Minh";
+    // console.log(getName());
+//Khai báo một arrow function có truyền vào tham số.
+    var getName = (name) => console.log(name);
+    getName("Võ Tuấn Minh");
+    
