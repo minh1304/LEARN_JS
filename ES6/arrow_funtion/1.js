9ab6a9cc34b2079f6108b4f44cacf26bbf85a291
minh1304 <vtm1304@gmail.com>
2022-09-05 23:09:44 +0700

    Update 1.js

--- ES6/arrow_funtion/1.js
+++ ES6/arrow_funtion/1.js
@@ -5,10 +5,21 @@
     // var getName = (name) => console.log(name);
     // getName("Võ Tuấn Minh");
 // Ví dụ với phương thức map
-    var listData = [
-        'Võ Tuấn Minh', 
-        'khmt2019',
-        21,
-    ];
-    listData.map(value => console.log(isNaN(value) === true ? value.toUpperCase():value + 1 ));
-    console.log(listData);
+    // var listData = [
+    //     'Võ Tuấn Minh', 
+    //     'khmt2019',
+    //     21,
+    // ];
+    // listData.map(value => console.log(isNaN(value) === true ? value.toUpperCase():value + 1 ));
+    // console.log(listData);
+
+//Ví dụ với phương thức foreach 
+    var ListNum = [1 , 2 ,3 ,4 ,5 ,9, 10];
+    var ahih = [];
+    ListNum.forEach(value => {
+        if (value %2 ==0 )
+        {
+            ahih.push(value);
+        }
+    });
+    console.log(ahih);
