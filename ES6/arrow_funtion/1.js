4caa5c041ecb593fdf9fb938419fe140f134a836
minh1304 <vtm1304@gmail.com>
2022-09-05 22:57:18 +0700

    Update 1.js

--- ES6/arrow_funtion/1.js
+++ ES6/arrow_funtion/1.js
@@ -2,6 +2,13 @@
     // var getName = () => "Võ Tuấn Minh";
     // console.log(getName());
 //Khai báo một arrow function có truyền vào tham số.
-    var getName = (name) => console.log(name);
-    getName("Võ Tuấn Minh");
-    
+    // var getName = (name) => console.log(name);
+    // getName("Võ Tuấn Minh");
+// Ví dụ với phương thức map
+    var listData = [
+        'Võ Tuấn Minh', 
+        'khmt2019',
+        21,
+    ];
+    listData.map(value => console.log(isNaN(value) === true ? value.toUpperCase():value + 1 ));
+    console.log(listData);
