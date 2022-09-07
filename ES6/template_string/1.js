2edb43371254db1cca7497ed405118140ed6e3d5
minh1304 <vtm1304@gmail.com>
2022-09-07 10:12:45 +0700

    Update 1.js

--- ES6/template_string/1.js
+++ ES6/template_string/1.js
@@ -2,8 +2,20 @@
     // var content = `I love UIT`;
     // console.log(content);
 // Khai báo biến nhiều dòng dưới dạng template literals
+    // var content = `<div>
+    //     <font color = "red">19521858</font> 
+    //     I love UIT.
+    //     </div>`;
+    // document.write(content);
+//Binding giá trị vào template literials
+    var infor = {
+        name: 'Võ Tuấn Minh',
+        id : 19521858,
+
+    }
     var content = `<div>
-        <font color = "red">19521858</font> 
+        ${infor.name} 
+        <font color = "red"> ${infor.id}</font> 
         I love UIT.
         </div>`;
     document.write(content);
