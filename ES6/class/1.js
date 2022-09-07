808429aa4720bb06e760790ff42a49a025570052
minh1304 <vtm1304@gmail.com>
2022-09-07 23:32:02 +0700

    Update 1.js
    Inheritance (Kế thừa)

--- ES6/class/1.js
+++ ES6/class/1.js
@@ -28,3 +28,17 @@ console.log( Employee.employeeName = "Võ Tuấn Minh");
 // let employee = new Employee("Tuấn Minh", 21);
 
 // // console.log(employee.showInfo());
+class Employeeeee extends Employee {
+    constructor(name,age,id) {
+        super(name,age);
+        this.id = id;
+    }
+    showInfo(){
+        return super.showInfo() + " - " +this.id;
+    }
+
+}
+
+let employee = new Employeeeee("Tuấn Minh", 21,19212312);
+
+console.log(employee.showInfo());
