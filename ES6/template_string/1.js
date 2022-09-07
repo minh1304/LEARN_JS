f84f622cced30ee8436c50e457ba3b229afdc301
minh1304 <vtm1304@gmail.com>
2022-09-07 10:07:28 +0700

    Update 1.js

--- ES6/template_string/1.js
+++ ES6/template_string/1.js
@@ -1,4 +1,9 @@
 // Khai báo biến dưới dạng template literals
-    var content = `I love UIT`;
-    console.log(content);
+    // var content = `I love UIT`;
+    // console.log(content);
 // Khai báo biến nhiều dòng dưới dạng template literals
+    var content = `<div>
+        <font color = "red">19521858</font> 
+        I love UIT.
+        </div>`;
+    document.write(content);
