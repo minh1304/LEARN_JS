9df1a93199e2f6400ac60ed0f4d6cc3f1ef9ae8b
minh1304 <vtm1304@gmail.com>
2022-09-07 11:11:35 +0700

    Update 1.js

--- ES6/enhenced_object/1.js
+++ ES6/enhenced_object/1.js
@@ -13,13 +13,32 @@
     //     console.log(index + ": " + student[index]);
     // }
 //Method Properties.
+    // var student = {
+    //     id: 19521858,
+    //     fname: "Võ Tuấn",
+    //     lname: "Minh",
+    //     fullname(){
+    //         return this.fname +" " + this.lname;
+    //     }
+
+    // }
+    // console.log(student.fullname());
     var student = {
         id: 19521858,
         fname: "Võ Tuấn",
         lname: "Minh",
         fullname(){
             return this.fname +" " + this.lname;
+        },
+        setname(fullname){
+            this.fullname = fullname;
+        },
+        getname(fullname){
+            return this.fullname;
         }
+
 
     }
     console.log(student.fullname());
+    student.setname("Phạm Văn T");
+    console.log(student.getname());
