1d22d7b568422f3babfc47e607c17a873523262a
minh1304 <vtm1304@gmail.com>
2022-09-07 11:38:58 +0700

    Update 1.js
    matching obj

--- ES6/destructing/1.js
+++ ES6/destructing/1.js
@@ -1,7 +1,17 @@
 //Arr matching 
-var list =[1 , 2, 3];
-var [a,b,c] = list;
-[a,c] = [c,a]
-console.log(a);
-console.log(b);
-console.log(c);
+    // var list =[1 , 2, 3];
+    // var [a,b,c] = list;
+    // [a,c] = [c,a]
+    // console.log(a);
+    // console.log(b);
+    // console.log(c);
+// Matching object 
+    var getobj =() =>{
+        return {        name: "Võ Tuấn Minh",
+        age: 21,
+        id: 19521858,};
+
+    };
+
+    var {name,age,id} = getobj();
+    console.log(name);
