e63e363e6fde612612dead0a7208aedb0b1c72e4
minh1304 <vtm1304@gmail.com>
2022-09-11 14:02:55 +0700

    Update 1.js

--- f8_JS_nangcao/IIFE_Scope_Closure/Cloure/1.js
+++ f8_JS_nangcao/IIFE_Scope_Closure/Cloure/1.js
@@ -35,14 +35,65 @@
 
 
 
-var getRandNumbers = function(min, max, length){
-    var result = [];
+// var getRandNumbers = function(min, max, length){
+//     var result = [];
 
-    for (var i = 0; i<length; i++)
+//     for (var i = 0; i<length; i++)
+//     {
+//          result.push(Math.floor(Math.random() * (max - min) + min ));
+//     }
+//     return result
+
+// }
+// console.log(getRandNumbers(2,9,5)); //2 tới 9 và có 5 phần tử
+
+
+
+
+// function getTotal(arr) {
+//     var result = 0;
+//     for(i = 0; i<arr.length; i++)
+//     {
+//         result += arr[i];
+//     }
+//     console.log(result);
+    
+// }
+// getTotal([1, 2, 3]) // Output: 6
+// getTotal([4, 5, -3]) // Output: 6
+// getTotal([4, 5, 3, 5]) // Output: 17
+
+
+var orders = [
     {
-         result.push(Math.floor(Math.random() * (max - min) + min ));
+        name: 'Khóa học HTML - CSS Pro',
+        price: 3000000
+    },
+    {
+        name: 'Khóa học Javascript Pro',
+        price: 2500000
+    },
+    {
+        name: 'Khóa học React Pro',
+        price: 3200000
     }
-    return result
+]
+var n_length = orders.length;
+
+var getTotal = function(arr){
+    var result = 0;
+    for (i =0; i< n_length; i++)
+    {
+        result += arr[i].price;
+    }
+
+    console.log(result); 
+    
 
 }
-console.log(getRandNumbers(2,9,5)); //2 tới 9 và có 5 phần tử
+
+// Expected results:
+// console.log(orders[1].price);
+
+
+getTotal(orders) // Output: 8700000
