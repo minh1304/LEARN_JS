2e9ea8a6ecce66aa5d9774703973070f9549e4bc
minh1304 <vtm1304@gmail.com>
2022-09-11 13:40:13 +0700

    Update 1.js

--- f8_JS_nangcao/IIFE_Scope_Closure/Cloure/1.js
+++ f8_JS_nangcao/IIFE_Scope_Closure/Cloure/1.js
@@ -1,11 +1,48 @@
-mathh = () =>{
-    let a = 0;
-    dem = () =>{
-        return ++a;
+// mathh = () =>{
+//     let a = 0;
+//     dem = () =>{
+//         return ++a;
+//     }
+//     return dem
+// }
+// var x  = mathh();
+// console.log(x());
+// console.log(x());
+// console.log(x());
+// 
+
+
+
+
+// tets khóa học F8
+// Viết hàm tại đây
+// function getLastElement(array){
+
+//     var last = array.length;
+
+//     return array[last-1]
+// }
+
+
+
+
+// // Ví dụ sử dụng
+// var animals = ['Monkey', 'Tiger', 'Elephant'];
+// var result = getLastElement(animals);
+
+// console.log(result); // Expected: "Elephant"
+// console.log(animals); // Expected: ['Monkey', 'Tiger', 'Elephant']
+
+
+
+var getRandNumbers = function(min, max, length){
+    var result = [];
+
+    for (var i = 0; i<length; i++)
+    {
+         result.push(Math.floor(Math.random() * (max - min) + min ));
     }
-    return dem
+    return result
+
 }
-var x  = mathh();
-console.log(x());
-console.log(x());
-console.log(x());
+console.log(getRandNumbers(2,9,5)); //2 tới 9 và có 5 phần tử
