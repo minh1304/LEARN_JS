78057dc6b6feed0f0f6fd9ea83ab0a0cf13d7321
minh1304 <vtm1304@gmail.com>
2022-09-14 23:55:06 +0700

    bt thêm xóa sửa courses

--- f8_JS_nangcao/BTcoban/json_sever/app.js
+++ f8_JS_nangcao/BTcoban/json_sever/app.js
@@ -1,9 +1,62 @@
-var courseApi = 'http://localhost:3000/courses';
 
-fetch(courseApi)
-    .then(response=>{
-        return response.json();
-    })
-    .then(course=>{
-        console.log(course);
-    })
+
+// fetch(courseApi)
+//     .then(response=>{
+//         return response.json();
+//     })
+//     .then(course=>{
+//         console.log(course);
+//     })
+
+
+
+
+var coursesApi = 'http://localhost:3000/courses';
+
+function start() {
+    getCourses(renderCourses);
+    handleCreateForm();
+    
+
+}
+start();
+
+
+//function
+
+function getCourses(callback){
+    fetch(coursesApi)
+        .then(response=>{
+            return response.json();
+        })
+        .then(callback)
+} 
+
+
+
+
+function renderCourses(courses){
+    var listCoursesBlock = 
+        document.querySelector('#list-courses');
+    var htmls = courses.map(course =>{
+        return `
+         <li>
+            <h4>${course.name}</h4>
+            <p> ${course.description}</p>
+         </li>
+        
+        `;
+        
+    });
+    listCoursesBlock.innerHTML = htmls.join('');
+}
+
+function handleCreateForm(){
+    var createBtn = document.querySelector('#create');
+    createBtn.onclick = () =>{
+        var name = document.querySelector('input[name="name"]').value;
+        var description = document.querySelector('input[name="description"]').value;
+    }
+
+
+}
