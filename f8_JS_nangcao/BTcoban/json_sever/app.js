9cc08e8c6a063607869f9ad886781da223a5741f
minh1304 <vtm1304@gmail.com>
2022-09-15 10:59:15 +0700

    chức năng xóa

--- f8_JS_nangcao/BTcoban/json_sever/app.js
+++ f8_JS_nangcao/BTcoban/json_sever/app.js
@@ -31,21 +31,58 @@ function getCourses(callback){
         })
         .then(callback)
 } 
-function createCourse(date){
-    
+function createCourse(data, callback){
+    var option = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+
+    }
+    fetch(coursesApi,option)
+        .then(response=>{
+            return response.json();
+        })
+        .then(callback)
+} 
+function handleDeleteCourse(id){
+    var option = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+
+    }
+    //như trên postman 
+    fetch(coursesApi + '/' + id,option)
+        .then(response=>{
+            return response.json();
+        })
+        .then(function(){
+            // Gọi lại API, tránh trường hợp này
+            // getCourses(renderCourses)  
+            var courseItem = document.querySelector('.course-item-' +id);
+            if(courseItem){
+                courseItem.remove();
+            }
+
+        })
+
 }
-
-
+    
+    
 
 function renderCourses(courses){
     var listCoursesBlock = 
         document.querySelector('#list-courses');
     var htmls = courses.map(course =>{
         return `
-         <li>
+         <li class = "course-item-${course.id}">
             <h4>${course.name}</h4>
             <p> ${course.description}</p>
-         </li>
+            <button onclick="handleDeleteCourse(${course.id})">Xóa;</button>
+         </li>  
         
         `;
         
@@ -58,6 +95,14 @@ function handleCreateForm(){
     createBtn.onclick = () =>{
         var name = document.querySelector('input[name="name"]').value;
         var description = document.querySelector('input[name="description"]').value;
+        var formData = {
+            name: name,
+            description: description,
+        }
+        createCourse(formData,function(){
+            getCourses(renderCourses)
+        })
+    
     }
 
 
