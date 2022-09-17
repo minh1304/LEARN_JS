2601db001395579504e455b6cf98741dc60049e7
minh1304 <vtm1304@gmail.com>
2022-09-17 16:02:27 +0700

    update music

--- f8_JS_nangcao/BTcoban/CodeMusicPlayer/app.js
+++ f8_JS_nangcao/BTcoban/CodeMusicPlayer/app.js
@@ -21,43 +21,41 @@ const app = {
         {
           name: "Tu Phir Se Aana",
           singer: "Raftaar x Salim Merchant x Karma",
-          path: "https://mp3.vlcmusic.com/download.php?track_id=34213&format=320",
+          path: "./assets/music/Tu Phir Se Aana-Raftaar -VlcMusic.CoM.mp3",
           image:
             "https://1.bp.blogspot.com/-kX21dGUuTdM/X85ij1SBeEI/AAAAAAAAKK4/feboCtDKkls19cZw3glZWRdJ6J8alCm-gCNcBGAsYHQ/s16000/Tu%2BAana%2BPhir%2BSe%2BRap%2BSong%2BLyrics%2BBy%2BRaftaar.jpg"
         },
         {
           name: "Naachne Ka Shaunq",
           singer: "Raftaar x Brobha V",
-          path:
-            "https://mp3.filmysongs.in/download.php?id=Naachne Ka Shaunq Raftaar Ft Brodha V Mp3 Hindi Song Filmysongs.co.mp3",
+          path: "./assets/music/Naachne Ka Shaunq-Brodha V-VlcMusic.CoM.mp3",          
           image: "https://i.ytimg.com/vi/QvswgfLDuPg/maxresdefault.jpg"
         },
         {
           name: "Mantoiyat",
           singer: "Raftaar x Nawazuddin Siddiqui",
-          path: "https://mp3.vlcmusic.com/download.php?track_id=14448&format=320",
+          path: "./assets/music/Mantoiyat-Raftaar -VlcMusic.CoM.mp3",          
           image:
             "https://a10.gaanacdn.com/images/song/39/24225939/crop_480x480_1536749130.jpg"
         },
         {
           name: "Aage Chal",
           singer: "Raftaar",
-          path: "https://mp3.vlcmusic.com/download.php?track_id=25791&format=320",
+          path: "./assets/music/Aage Chal-Raftaar -VlcMusic.CoM.mp3",          
           image:
             "https://a10.gaanacdn.com/images/albums/72/3019572/crop_480x480_3019572.jpg"
         },
         {
           name: "Damn",
           singer: "Raftaar x kr$na",
-          path:
-            "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
+          path: "./assets/music/Damn - Raftaar-(MastiMusic.Com).mp3",          
           image:
             "https://i.scdn.co/image/ab67616d0000b273441105b2aed66a31a869299f"
         },
         {
           name: "Feeling You",
           singer: "Raftaar x Harjas",
-          path: "https://mp3.vlcmusic.com/download.php?track_id=27145&format=320",
+          path: "./assets/music/Feeling You (Mr Nair)-Raftaar -VlcMusic.CoM.mp3",
           image:
             "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp"
         }
