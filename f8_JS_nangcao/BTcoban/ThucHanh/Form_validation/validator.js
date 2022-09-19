f8f39533d4178c817b147f93d4b49a7f48550d12
minh1304 <vtm1304@gmail.com>
2022-09-19 15:10:40 +0700

    xong: không hiện lỗi khi người dùng đang nhập

--- f8_JS_nangcao/BTcoban/ThucHanh/Form_validation/validator.js
+++ f8_JS_nangcao/BTcoban/ThucHanh/Form_validation/validator.js
@@ -13,9 +13,6 @@ function Validator(options) {
             errorElement.innerText = '';
             inputElement.parentElement.classList.remove('invalid');
         }
-
-
-
     }
     var formElement = document.querySelector(options.form);
     if(formElement)
@@ -25,15 +22,19 @@ function Validator(options) {
             var inputElement = formElement.querySelector(rule.selector);
             //parentElement: Lấy ra element cha từ element con
             if(inputElement) {
+                //XỬ lý blur khỏi input
                 inputElement.onblur = function() {
                     validate(inputElement, rule);
                 }
-
-            }
-            
+                //Xử lý trường hợp khi người dùng đang nhập 
+                inputElement.oninput = function() {
+                    var errorElement = inputElement.parentElement.querySelector('.form-message');
+                    errorElement.innerText = '';
+                    inputElement.parentElement.classList.remove('invalid');
+                }
+            }        
         });
     }
-
 }
 //Định nghĩa rules
 //Khi có lỗi => message lỗi
@@ -43,19 +44,17 @@ Validator.isRequired = function(selector) {
         selector: selector,
         //Kiểm tra người dụng đã nhập chưa
         test: function(value) {
-            return value.trim() ? undefined : 'vui lòng nhập trường này'
-
+            return value.trim() ? undefined : 'Vui lòng nhập trường này'
         }
-
-    }
-    
+    }  
 }
 Validator.isMail = function(selector) {
     return {
         selector: selector,
         //Kiểm tra có phải mail  hay kh
-        test: function() {
-            
+        test: function(value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+            return regex.test(value) ? undefined : 'Vui lòng nhập đúng email'
         }
 
     }
