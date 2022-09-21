dcbc20b67f2b1f1a367b218ab47710d79e22e46e
minh1304 <vtm1304@gmail.com>
2022-09-22 01:00:12 +0700

    end

--- f8_JS_nangcao/codebuildUI/script.js
+++ f8_JS_nangcao/codebuildUI/script.js
@@ -1,9 +1,4 @@
-import html from './core.js'
-const cars = ['BMW' , 'Porsche' , 'Mercedes']
-const isSuccess = false;
-const output =html`
-    <h1>${false}</h1>
-    <ul>
-        ${cars.map(car =>`<li>${car} </li>`).join('')}
-    </ul>`
-console.log(output);
+import { attach } from "./store.js";
+import App from './component/App.js';
+
+attach(App, document.getElementById('root'))
