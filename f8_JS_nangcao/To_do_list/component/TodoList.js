9927a3d3f704978f21806e6f434fb6a5221bd3a4
minh1304 <vtm1304@gmail.com>
2022-09-22 16:50:13 +0700

    upload

--- f8_JS_nangcao/To_do_list/component/TodoList.js
+++ f8_JS_nangcao/To_do_list/component/TodoList.js
@@ -1,28 +1,16 @@
 import html from "../core.js";
+import TodoItem from './TodoItem.js';
 function TodoList() {
     return html`
-        <section class="main">
-            <input id="toggle-all" class="toggle-all" type="checkbox">
-            <label for="toggle-all">Mark all as complete</label>
-            <ul class="todo-list">
-                <li class="completed">
-                    <div class="view">
-                        <input class="toggle" type="checkbox" checked>
-                        <label>Taste JavaScript</label>
-                        <button class="destroy"></button>
-                    </div>
-                    <input class="edit" value="Create a TodoMVC template">
-                </li>
-                <li>
-                    <div class="view">
-                        <input class="toggle" type="checkbox">
-                        <label>Buy a unicorn</label>
-                        <button class="destroy"></button>
-                    </div>
-                    <input class="edit" value="Rule the web">
-                </li>
-            </ul>
-        </section>
+    <section class="main">
+        <input id="toggle-all" class="toggle-all" type="checkbox">
+        <label for="toggle-all">Mark all as complete</label>
+        <ul class="todo-list">
+            ${TodoItem()}
+            ${TodoItem()}
+            ${TodoItem()}
+        </ul>
+    </section>
         `
 }
 export default TodoList
